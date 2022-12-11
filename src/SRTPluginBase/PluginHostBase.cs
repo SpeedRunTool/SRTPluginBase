34e850c6b324149f89cf06b6c33e54550e79712b
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2022-12-11 17:16:33 -0600

    Update PluginHostBase.

--- src/SRTPluginBase/PluginHostBase.cs
+++ src/SRTPluginBase/PluginHostBase.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SRTPluginBase
 {
@@ -10,9 +8,6 @@ namespace SRTPluginBase
     {
         protected IDictionary<string, IPluginStateValue<IPlugin>> loadedPlugins = new Dictionary<string, IPluginStateValue<IPlugin>>(StringComparer.OrdinalIgnoreCase);
         public IReadOnlyDictionary<string, IPluginStateValue<IPlugin>> LoadedPlugins => loadedPlugins.AsReadOnly();
-
-        public abstract Task StartAsync(CancellationToken cancellationToken);
-        public abstract Task StopAsync(CancellationToken cancellationToken);
 
         public T? GetPluginReference<T>(string pluginName) where T : class, IPlugin
         {
