088dbe4aa0e756f478114b0c297a82ace2d80732
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-05-22 22:52:13 -0500

    Remove T generic from IPluginProducer. >.>

--- src/SRTPluginBase/IPluginProducer.cs
+++ src/SRTPluginBase/IPluginProducer.cs
@@ -2,24 +2,14 @@
 
 namespace SRTPluginBase
 {
-    public interface IPluginProducer<T> : IPlugin, IEquatable<IPluginProducer<T>>
+    public interface IPluginProducer : IPlugin, IEquatable<IPluginProducer>
     {
         /// <summary>
-        /// Requests the producer plugin to update the Data property.
+        /// Requests the producer plugin to return new data.
         /// </summary>
-        void Refresh();
+        object? Refresh();
 
-        /// <summary>
-        /// The most recent plugin-specific data structure for this producer.
-        /// </summary>
-        T? Data { get; }
-
-        /// <summary>
-        /// The DateTime representing the last time the Data property was updated. This value is null if the Data property has not been populated.
-        /// </summary>
-        DateTime? LastUpdated { get; }
-
-        public new bool Equals(IPluginProducer<T>? other) => (this as IPlugin).Equals(other);
+        public new bool Equals(IPluginProducer? other) => (this as IPlugin).Equals(other);
 
         public new int GetHashCode() => (this as IPlugin).GetHashCode();
     }
