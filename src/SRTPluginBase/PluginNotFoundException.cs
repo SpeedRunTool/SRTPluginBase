616a9edc4c3ee1b5e1cec7e0aff20f69e76dc4fc
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-05-24 18:36:14 -0500

    PluginNotFoundException.

---
+++ src/SRTPluginBase/PluginNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SRTPluginBase
+{
+	/// <summary>
+	/// Thrown when a required plugin cannot be found when querying an IPluginHost.
+	/// </summary>
+	public class PluginNotFoundException : Exception
+	{
+		public string PluginName { get; private set; }
+
+		public PluginNotFoundException(string pluginName) : base() => (PluginName) = (pluginName);
+	}
+}
