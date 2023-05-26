0fccf4d74f965f014243c2180f72f07461e617d4
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-05-25 23:22:23 -0500

    Added some json classes for auto updates and a marketplace of sorts (search and download plugins!).
    Also added some pages and such. Wee!

---
+++ src/SRTPluginBase/ManifestJson.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SRTPluginBase
+{
+    public class ManifestJson
+    {
+        public IEnumerable<string> Contributors { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
---
+++ src/SRTPluginBase/MasterJson.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace SRTPluginBase
+{
+    internal class MasterJson
+    {
+        internal MasterHostEntry Host { get; set; }
+        internal IEnumerable<MasterPluginEntry> Plugins { get; set; }
+    }
+
+    internal class MasterHostEntry
+    {
+        internal string ManifestURL { get; set; }
+    }
+
+    internal class MasterPluginEntry
+    {
+        internal string Name { get; set; }
+        internal MasterPluginPlatformEnum Platform { get; set; }
+        internal MasterPluginTypeEnum Type { get; set; }
+        internal IEnumerable<string> Categories { get; set; }
+        internal string ManifestURL { get; set; }
+    }
+
+    internal enum MasterPluginPlatformEnum : int
+    {
+        Any, // 0
+        x86, // 1
+        x64, // 2
+    }
+
+    internal enum MasterPluginTypeEnum : int
+    {
+        Producer, // 0
+        Consumer, // 1
+    }
+}
