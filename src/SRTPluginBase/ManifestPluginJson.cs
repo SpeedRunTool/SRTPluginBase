2cced453f60151c52d4355b6a1855e85c04cd8b2
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2024-03-14 21:01:04 -0500

    Added new properties from DeathHound's SRTHost commit. Ref: 4784960b4165d44d7393a5ba6edd15a3481c5907

--- src/SRTPluginBase/ManifestPluginJson.cs
+++ src/SRTPluginBase/ManifestPluginJson.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SRTPluginBase
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public class ManifestPluginJson
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Uri RepoURL { get; set; }
         public IEnumerable<string> Contributors { get; set; }
         public IEnumerable<string> Tags { get; set; }
         public IEnumerable<ManifestReleaseJson> Releases { get; set; }
